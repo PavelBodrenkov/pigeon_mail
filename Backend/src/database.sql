669e668c803a17aa3cd4018199bf245bf0306a3f
Павел <bodrenkov_pv@ed-it.ru>
2022-12-07 15:35:35 +0300

    Получение и создание диологов на бэк

--- Backend/src/database.sql
+++ Backend/src/database.sql
@@ -5,46 +5,115 @@ create TABLE users(
     avatar VARCHAR(255),
     fullname VARCHAR(255),
     password VARCHAR(255) DEFAULT(null),
-    is_admin INTEGER DEFAULT(0),
+    is_admin SMALLINT DEFAULT(0),
     confirmed DATE DEFAULT(null),
     confirmed_hash VARCHAR(255),
     activation_link VARCHAR(255),
     is_activation INTEGER DEFAULT(0),
-    last_seen DATE,
-    created_at DATE DEFAULT(null),
-    updated_at DATE DEFAULT(null),
-    deleted_at DATE DEFAULT(null)
+    last_seen TIMESTAMP,
+    created_at TIMESTAMP DEFAULT(null),
+    updated_at TIMESTAMP DEFAULT(null),
+    deleted_at TIMESTAMP DEFAULT(null)
 )
 
-create TABLE dialogs(
+-- Диологи
+create TABLE conversation (
     id SERIAL PRIMARY KEY,
-    owner INTEGER,
-    partner INTEGER,
-    created_at DATE DEFAULT(null),
-    updated_at DATE DEFAULT(null),
-    deleted_at DATE DEFAULT(null),
-    FOREIGN KEY (owner) REFERENCES users (id)
+    first INTEGER NOT NULL,
+    second INTEGER NOT NULL,
+    last_message_id INTEGER NOT NULL,
+    sender INTEGER NOT NULL,
+    first_delete SMALLINT NOT NULL,
+    second_delete SMALLINT NOT NULL,
+    unread INTEGER NOT NULL
 )
 
-create TABLE messages(
+create TABLE messages (
     id SERIAL PRIMARY KEY,
-    owner INTEGER,
-    partner INTEGER,
-    text VARCHAR(255),
-    dialogId INTEGER,
-    unreaded:INTEGER,
-    created_at DATE DEFAULT(null),
-    updated_at DATE DEFAULT(null),
-    deleted_at DATE DEFAULT(null),
-    FOREIGN KEY (dialogId) REFERENCES dialogs (id)
+    conv_id INTEGER NOT NULL,
+    sender INTEGER NOT NULL,
+    addressee INTEGER NOT NULL,
+    readed SMALLINT NOT NULL,
+    sender_delete SMALLINT NOT NULL,
+    addressee_delete SMALLINT NOT NULL,
+    message text,
+    date TIMESTAMP
 )
 
 create TABLE tokens(
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER,
-    refresh_token VARCHAR(1000),
-    created_at DATE DEFAULT(null),
-    updated_at DATE DEFAULT(null),
-    deleted_at DATE DEFAULT(null),
-    FOREIGN KEY (user_id) REFERENCES users (id)
+   id SERIAL PRIMARY KEY,
+   user_id INTEGER,
+   refresh_token VARCHAR(1000),
+   created_at TIMESTAMP DEFAULT(null),
+   updated_at TIMESTAMP DEFAULT(null),
+   deleted_at TIMESTAMP DEFAULT(null),
+   FOREIGN KEY (user_id) REFERENCES users (id)
 )
+------------------------------------------------------------------------------------
+-- Таблица со списком групп чата
+--     create TABLE chats(
+--         id SERIAL PRIMARY KEY,
+--         name VARCHAR(255),
+--         user_id INTEGER,
+--         created_at TIMESTAMP DEFAULT(null),
+--         updated_at TIMESTAMP DEFAULT(null),
+--         deleted_at TIMESTAMP DEFAULT(null)
+--         FOREIGN KEY (user_id) REFERENCES users (id)
+--     )
+--
+-- -- Таблица со списком участников чата
+--     create TABLE party(
+--         id SERIAL PRIMARY KEY,
+--         chat_id INTEGER,
+--         user_id INTEGER,
+--         created_at TIMESTAMP DEFAULT(null),
+--         updated_at TIMESTAMP DEFAULT(null),
+--         deleted_at TIMESTAMP DEFAULT(null)
+--     )
+--
+--     create TABLE messages(
+--         messages_id SERIAL PRIMARY KEY,
+--         chat_id INTEGER,
+--         user_id INTEGER,
+--         content VARCHAR(1000),
+--         created_at TIMESTAMP DEFAULT(null),
+--         updated_at TIMESTAMP DEFAULT(null),
+--         deleted_at TIMESTAMP DEFAULT(null)
+--         FOREIGN KEY (user_id) REFERENCES users (id)
+--     )
+--
+--     create TABLE messages_status(
+--         id SERIAL PRIMARY KEY,
+--         message_id INTEGER,
+--         user_id INTEGER,
+--         is_read SMALLINT,
+--         created_at TIMESTAMP DEFAULT(null),
+--         updated_at TIMESTAMP DEFAULT(null),
+--         deleted_at TIMESTAMP DEFAULT(null)
+--     )
+
+-------------------------------------------------------------
+
+-- create TABLE dialogs(
+--     id SERIAL PRIMARY KEY,
+--     owner INTEGER,
+--     partner INTEGER,
+--     created_at DATE DEFAULT(null),
+--     updated_at DATE DEFAULT(null),
+--     deleted_at DATE DEFAULT(null),
+--     FOREIGN KEY (owner) REFERENCES users (id)
+-- )
+--
+-- create TABLE messages(
+--     id SERIAL PRIMARY KEY,
+--     owner INTEGER,
+--     partner INTEGER,
+--     text VARCHAR(255),
+--     dialogId INTEGER,
+--     unreaded:INTEGER,
+--     created_at DATE DEFAULT(null),
+--     updated_at DATE DEFAULT(null),
+--     deleted_at DATE DEFAULT(null),
+--     FOREIGN KEY (dialogId) REFERENCES dialogs (id)
+-- )
+
--- Backend/src/services/dialog.service.js
+++ Backend/src/services/dialog.service.js
@@ -3,17 +3,76 @@ const db = require('../db');
 class DialogService {
 
     async getAllDialogsByUser(req) {
-        const {id} = req.user
-        const sql = `SELECT * FROM dialogs WHERE owner = $1`
-        const dialogs = db.query(sql, [id])
-        return dialogs.rows
+        // const {id} = req.user
+        const id = 3
+        const sql =
+            `
+            SELECT U.id as userId, U.fullname, U.avatar, C.id as convId, C.sender, C.unread, M.message, M.date
+            FROM users as U, conversation as C
+                LEFT JOIN messages as M ON(C.last_message_id = M.id)
+            WHERE (C.first = ${id} OR C.second = ${id})
+            AND CASE
+                WHEN C.first = ${id}
+                    THEN C.second = U.id AND C.first_delete = 0
+                WHEN C.second = ${id}
+                    THEN C.first = U.id AND C.second_delete = 0
+                END 
+            ORDER BY C.unread DESC
+            `;
+        const allDialog = await db.query(sql)
+        return allDialog.rows
     }
 
+
     async createDialog(req) {
-        const {owner, partner}  = req.body
-        const sql = `INSERT INTO dialogs (owner, partner, created_at) VALUES($1, $2, $3) RETURNING *`
-        const dialog = await db.query(sql, [owner, partner, new Date()])
-        return dialog.rows[0]
+        const {owner, partner} = req.body
+        const message = 'Hello My Friend three';
+        //Проверяем, не отправляем ли мы диалог сами себе
+        if (owner !== partner) {
+            //Проверяем есть ли уже диалог
+            const sql = `
+                SELECT id
+                FROM conversation
+                WHERE (first = ${owner} AND second = ${partner})
+                   OR (first = ${partner} AND second = ${owner})
+            `
+            const row_conversation = await db.query(sql);
+
+            let last_conversation_id = 0 //
+
+                //Если диалога нет, то создаем
+            if (row_conversation.rows.length === 0) {
+                const sql =
+                    `INSERT INTO conversation (first, second, last_message_id, sender, first_delete, second_delete,
+                                               unread)
+                     VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`;
+                const dialog = await db.query(sql, [owner, partner, 0, owner, 0, 0, 0])
+                last_conversation_id = dialog.rows[0].id
+            } else {
+                last_conversation_id = row_conversation.rows[0].id
+            }
+
+            //Создаем сообщение
+            const messageSQL = `INSERT INTO messages (conv_id, sender, addressee, readed, sender_delete,
+                                                      addressee_delete, message, date)
+                                VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`
+            const row_messages = await db.query(messageSQL, [last_conversation_id, owner, partner, 0, 0, 0, message, new Date()])
+
+            //обновляем таблицу с диалогами
+            const updateConversationSQL = `
+                UPDATE conversation
+                SET last_message_id = ${row_messages.rows[0].id},
+                    sender          = ${owner},
+                    unread          = (SELECT COUNT(*)
+                                       FROM messages as M
+                                       WHERE M.conv_id = ${last_conversation_id}
+                                         AND M.readed = 0
+                                         AND M.sender = ${owner})
+                WHERE id = ${last_conversation_id} RETURNING *
+            `
+            const result = await db.query(updateConversationSQL);
+            return result.rows[0]
+        }
     }
 
 }
